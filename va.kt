2d3028d2375cc71bc6485f051c2912809fa34def
LongKhoa0706 <tailieuhay0706@gmail.com>
2020-04-03 12:15:53 +0700

    first commit

---
+++ va.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+   
+    //mutable , imutable
+    var email : String = "Hell111oWord"
+
+    email = "Longkhoa"
+    
+    val name  = "abc@gmail.com"
+    println(name);
+
+}
